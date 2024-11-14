e21837710494ce78da1da3901e29711a243ff3c9
Diana Edith Collantes Esinoza <dianacollantespinozza@gmail.com>
2024-11-14 12:52:09 -0500

    PRIMERA ACTUALIZACION GRUPAL

--- PROYECTO IC 01/PROYECTO IC 01/Program.cs
+++ PROYECTO IC 01/PROYECTO IC 01/Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("Mi nombre es Diana");
 Console.WriteLine("Mi apellido es Espinoza");
 Console.WriteLine("DANTE CANTANTE");
 Console.WriteLine("DANTE COCHE");
+Console.WriteLine("EL FERNAMDO ES INFIEL JJAJJA");
