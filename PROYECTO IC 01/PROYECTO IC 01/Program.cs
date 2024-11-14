165914aa0960e8389835022e64afcffde42b4840
Diana Edith Collantes Esinoza <dianacollantespinozza@gmail.com>
2024-11-14 12:21:56 -0500

    SEGUNDA ACTUALIZACIÓN

--- PROYECTO IC 01/PROYECTO IC 01/Program.cs
+++ PROYECTO IC 01/PROYECTO IC 01/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Mi nombre es Diana"); 
+Console.WriteLine("Mi nombre es Diana");
+
+
+Console.WriteLine("Mi apellido es Espinoza");
